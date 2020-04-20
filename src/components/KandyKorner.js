3946b3b8004dc50e24f452850c833d9f74059b2f
Keartroth <mecarrolljr@gmail.com>
2020-04-20 15:21:23 -0500

    initial commit

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <small>Get Your Kandy Korn at the Korner!</small>
+        <address>
+            <div>Visit Us at the East Nashville Location</div>
+            <div>500 Kandy Lane</div>
+        </address>
+    </>
+)
