71b74af6089c5106c9619a3fe520c3126ca69b49
Keartroth <mecarrolljr@gmail.com>
2020-04-22 19:13:40 -0500

    added the following files: Login.css, Login.js, Register.js; refactored NavBar to display a log out button; refactored KandyKorner.js to check for a local storage item to fake user login and authentication.

---
+++ src/components/auth/Login.js
@@ -0,0 +1,73 @@
+import React, { useRef } from "react"
+import { Button } from 'reactstrap'
+import { Link } from "react-router-dom";
+import "./Login.css"
+
+
+const Login = props => {
+    const email = useRef()
+    const password = useRef()
+
+    const existingUserCheck = () => {
+        return fetch(`http://localhost:8088/customers?email=${email.current.value}`)
+            .then(_ => _.json())
+            .then(user => {
+                if (user.length) {
+                    return user[0]
+                }
+                return false
+            })
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        existingUserCheck()
+            .then(exists => {
+                if (exists && exists.password === password.current.value) {
+                    localStorage.setItem("kandy_customer", exists.id)
+                    props.history.push("/")
+                } else if (exists && exists.password !== password.current.value) {
+                    window.alert("Password does not match.")
+                } else if (!exists) {
+                    window.alert("Combination of email or password does not exist.")
+                }
+            })
+    }
+
+    return (
+        <main className="container--login">
+            <section>
+                <form className="form--login" onSubmit={handleLogin}>
+                    <h1>Kandy Korner</h1>
+                    <h2>Please sign in</h2>
+                    <fieldset>
+                        <label htmlFor="inputEmail"> Email address </label>
+                        <input ref={email} type="email"
+                            id="email"
+                            className="form-control"
+                            placeholder="Email address"
+                            required autoFocus />
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword"> Password </label>
+                        <input ref={password} type="password"
+                            id="password"
+                            className="form-control"
+                            placeholder="Password"
+                            required />
+                    </fieldset>
+                    <fieldset>
+                        <Button type="submit">
+                            Sign in
+                        </Button>
+                    </fieldset>
+                </form>
+            </section>
+            <section className="link--register">
+                <Link to="/register">Not a member yet?</Link>
+            </section>
+        </main>
+    )
+}
+export default Login
