488c0b77cf3701c5334cda8b9bcb22777ee53602
Keartroth <mecarrolljr@gmail.com>
2020-04-24 18:29:50 -0500

    added a modal to CartItem module to allow user to edit the current quantity of any product in the cart; refactored employee module to remove incorrect autofocus on employee form.

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,10 +1,13 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useRef } from "react"
 import "../customers/Customer.css"
-import { Button } from "reactstrap"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 import { CartContext } from "./CartProductProvider"
 
 export default (props) => {
-    const { shoppingCart, removeItemFromCart } = useContext(CartContext)
+    const [modal, setModal] = useState(false)
+    const toggle = () => setModal(!modal)
+    const { shoppingCart, removeItemFromCart, addItemToCart } = useContext(CartContext)
+    const productQuantityNode = useRef(0)
     const customerId = parseInt(localStorage.getItem("kandy_customer"))
     const product = props.product
     const totalPrice = product.quanitiy * product.price
@@ -12,8 +15,37 @@ export default (props) => {
     const removeItem = () => {
         const currentCustomersCart = shoppingCart.filter(sc => sc.customerId === customerId)
         const foundProducts = currentCustomersCart.filter(cc => cc.productId === product.id)
-        debugger
         foundProducts.forEach(fp => removeItemFromCart(fp.id))
+    }
+
+    const editCartQuantity = () => {
+        const productQuantity = productQuantityNode.current.value
+        if (productQuantity > product.quanitiy) {
+            const numberToAdd = productQuantity - product.quanitiy
+            const productId = props.product.id
+            const customerId = parseInt(localStorage.getItem("kandy_customer"))
+
+            for (var i = 0; i < numberToAdd; i++) {
+                let productObject = {
+                    productId: productId,
+                    customerId: customerId
+                }
+                addItemToCart(productObject)
+            }
+            toggle()
+        } else if (productQuantity < product.quanitiy) {
+            const numberToRemove = product.quanitiy - productQuantity
+            const currentCustomersCart = shoppingCart.filter(sc => sc.customerId === customerId)
+            const foundProducts = currentCustomersCart.filter(cc => cc.productId === product.id)
+
+            for (var i = 1; i <= numberToRemove; i++) {
+                removeItemFromCart(foundProducts[i].id)
+            }
+            toggle()
+        } else {
+            toggle()
+            alert("Warning: updated quantity is identical to original quantity. Please ensure you are ordering the desired amount.")
+        }
     }
 
     return (
@@ -23,8 +55,32 @@ export default (props) => {
                 <td>{product.quanitiy}</td>
                 <td>{product.price}</td>
                 <td>{totalPrice.toFixed(2)}</td>
-                <td><Button onClick={removeItem}>Remove Items From Cart</Button></td>
+                <td><Button color="secondary" onClick={toggle}>Edit Quantity</Button><Button onClick={removeItem}>Remove Items From Cart</Button></td>
             </tr>
+
+            <Modal className="cart__edit" isOpen={modal} toggle={toggle}>
+                <ModalHeader>
+                    {product.name}
+                </ModalHeader>
+                <ModalBody>
+                    <div className="cart__edit-quantity">
+                        <label htmlFor="cart__edit-quantityInput">Quantity to Purchase: </label>
+                        <input
+                            type="number"
+                            id="cart__edit-quantityInput"
+                            ref={productQuantityNode}
+                            required
+                            autoFocus
+                            className="cart__edit-quantity"
+                            defaultValue={product.quanitiy}
+                        />
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={editCartQuantity}>Update Quantity</Button>
+                    <Button color="secondary" onClick={toggle}>Close</Button>
+                </ModalFooter>
+            </Modal>
         </>
     )
 }
